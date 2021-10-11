ecf098eb9767bee5153d6d5f95decc4a64fdbe79
Drake-Robertson <91571501+Drake-Robertson@users.noreply.github.com>
2021-10-11 11:07:13 -0500

    Update 10/11/21 1107
    This is a test of an update

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,6 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("This is a projest to test. using initial commit and push a project to GitHub.");
+		System.out.println("update 1");
 	}
 
 }
